8fa3cc5e5d130cdc32cc07fffbf41baf659af5ad
aocastro <adriano.castro2@etec.sp.gov.br>
2021-04-27 11:10:49 -0300

    Commit da tela fe gerenciamento de eixo

---
+++ src/conexao/conn.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados....
+    $hostname = "fdb22.awardspace.net";
+    $dbname = "2874836_library";
+    $username = "2874836_library";
+    $password = "Et3cL1br@ry";
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo 'Conexão realizada com sucesso!';
+    } catch (PDOException $e) {
+        echo 'Error: '.$e->getMessage();
+    }
+
---
+++ src/eixo/controller/new-eixo.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('#modal-eixo').modal('show')
+    })
+})
